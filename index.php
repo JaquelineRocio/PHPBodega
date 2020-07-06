a5b0fbed81899acd7c05ad9e26dc4ec827bcacdb
JaquelineRocio <67769601+JaquelineRocio@users.noreply.github.com>
2020-07-06 11:00:25 -0500

    v1

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>BODEGA</title>
+</head>
+<body>
+	<div class="text-center">
+    <h1 class="display-4">Bienvenido a Preguntas Frecuentes con un Chatbot para una bodega</h1>
+   
+	<div>
+		<iframe src='https://webchat.botframework.com/embed/qnamakerbodega-bot?s=WTYFWLGAlIw.gdkwsJefvvxX6gkfZrl0LwrZEY0lFdhOQGJhkReaz98'  style='min-width: 400px; width: 100%; min-height: 500px;'></iframe>
+	</div>
+</div>
+</body>
+</html>
